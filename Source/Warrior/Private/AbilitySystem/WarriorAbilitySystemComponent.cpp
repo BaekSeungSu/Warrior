72654e9714c6c7b3d267d3057922973f4888d8e3
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-09-25 12:17:23 +0900

    `WarriorAbilitySystemComponent에 AbilityInput Pressed/Released 처리 함수 추가`

--- Source/Warrior/Private/AbilitySystem/WarriorAbilitySystemComponent.cpp
+++ Source/Warrior/Private/AbilitySystem/WarriorAbilitySystemComponent.cpp
@@ -1,2 +1,16 @@
 #include "AbilitySystem/WarriorAbilitySystemComponent.h"
 
+void UWarriorAbilitySystemComponent::OnAbilityInputPressed(const FGameplayTag& InInputTag)
+{
+	if (!InInputTag.IsValid()) return;
+
+	for (const FGameplayAbilitySpec AbilitySpec : GetActivatableAbilities())
+	{
+		if (!AbilitySpec.GetDynamicSpecSourceTags().HasTagExact(InInputTag)) continue;
+		TryActivateAbility(AbilitySpec.Handle);
+	}
+}
+
+void UWarriorAbilitySystemComponent::OnAbilityInputReleased(const FGameplayTag& InInputTag)
+{
+}
