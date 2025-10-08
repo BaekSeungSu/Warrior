917d8e91b80cfecde9050d9bf0c43340f225c2b2
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-08 20:29:09 +0900

    `WarriorAbilitySystemComponent에 RemoveGrantedHeroWeaponAbilities 함수 추가`

--- Source/Warrior/Private/AbilitySystem/WarriorAbilitySystemComponent.cpp
+++ Source/Warrior/Private/AbilitySystem/WarriorAbilitySystemComponent.cpp
@@ -36,3 +36,21 @@ void UWarriorAbilitySystemComponent::GrantHeroWeaponAbilities(
 		OutGrantedAbilitySpecHandles.AddUnique(GiveAbility(AbilitySpec));
 	}
 }
+
+void UWarriorAbilitySystemComponent::RemoveGrantedHeroWeaponAbilities(
+	TArray<FGameplayAbilitySpecHandle>& InSpecHandlesToRemove)
+{
+	if (InSpecHandlesToRemove.IsEmpty())
+	{
+		return;
+	}
+
+	for (const FGameplayAbilitySpecHandle& SpecHandle : InSpecHandlesToRemove)
+	{
+		if (SpecHandle.IsValid())
+		{
+			ClearAbility(SpecHandle);
+		}
+	}
+	InSpecHandlesToRemove.Empty();
+}
