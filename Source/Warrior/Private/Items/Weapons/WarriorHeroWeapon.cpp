ac5bc0c4a70d1d11cddacb4cad0d06a7ef289087
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-08 20:29:22 +0900

    `WarriorHeroWeapon에 GrantedAbilitySpecHandles 관련 함수 및 멤버 변수 추가`

--- Source/Warrior/Private/Items/Weapons/WarriorHeroWeapon.cpp
+++ Source/Warrior/Private/Items/Weapons/WarriorHeroWeapon.cpp
@@ -1,5 +1,11 @@
-// Vince Petrelli All Right Reseved
-
-
 #include "Items/Weapons/WarriorHeroWeapon.h"
 
+void AWarriorHeroWeapon::AssignGrantedAbilitySpecHandles(const TArray<FGameplayAbilitySpecHandle>& InSpecHandles)
+{
+	GrantedAbilitySpecHandles = InSpecHandles;
+}
+
+TArray<FGameplayAbilitySpecHandle> AWarriorHeroWeapon::GetGrantedAbilitySpecHandles() const
+{
+	return GrantedAbilitySpecHandles;
+}
