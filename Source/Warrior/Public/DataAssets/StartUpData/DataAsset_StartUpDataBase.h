550c9d830c037013a1fa39b58a3b8bedceb4be5d
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-10-24 01:17:18 +0900

    `DataAsset_StartUpDataBase에 StartUpGameplayEffects 적용 로직 및 관련 멤버 변수 추가`

--- Source/Warrior/Public/DataAssets/StartUpData/DataAsset_StartUpDataBase.h
+++ Source/Warrior/Public/DataAssets/StartUpData/DataAsset_StartUpDataBase.h
@@ -6,6 +6,7 @@
 
 class UWarriorAbilitySystemComponent;
 class UWarriorGameplayAbility;
+class UGameplayEffect;
 
 UCLASS()
 class WARRIOR_API UDataAsset_StartUpDataBase : public UDataAsset
@@ -22,6 +23,9 @@ protected:
 	
 	UPROPERTY(EditDefaultsOnly, Category="StartUpData")
 	TArray<TSubclassOf<UWarriorGameplayAbility>> ReactiveAbilities;
+
+	UPROPERTY(EditDefaultsOnly, Category="StartUpData")
+	TArray<TSubclassOf<UGameplayEffect>> StartUpGameplayEffects;
 	
 	void GrantAbilities(const TArray<TSubclassOf<UWarriorGameplayAbility>>& InAbilitiesToGive, UWarriorAbilitySystemComponent* InASCToGive, int32 ApplyLevel = 1);
 };
